edae6fbad23dcd2b79e7d0cbdadd8adbb828aac0
jennikins813 <jennikins813@yahoo.com>
2016-03-22 12:36:40 -0700

    Adding Comment field to adventures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319185756) do
+ActiveRecord::Schema.define(version: 20160322192224) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160319185756) do
     t.date     "visit"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "adventure_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
